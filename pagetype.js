43cab9fc706863a91d8150c3a677341b81648254
doktertech <doktertech@gmail.com>
2015-10-27 18:47:43 +0700

    Create pagetype.js

---
+++ pagetype.js
@@ -0,0 +1,11 @@
+//<![CDATA[
+$(window).load(function(){
+relatedPostsWidget({
+containerSelector: "#relatedpostsjp",
+relatedTitle:"Related Posts",
+maxposts: 3,
+maxTags: 10,
+maxpostsPerTag: 10,
+});
+});
+//]]>
