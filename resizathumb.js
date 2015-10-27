5817faefcb78cd8cb905bff816d07c8eff495fa2
doktertech <doktertech@gmail.com>
2015-10-27 18:43:09 +0700

    Create resizathumb.js

---
+++ resizathumb.js
@@ -0,0 +1,3 @@
+//<![CDATA[
+function resizeThumb(d,c){for(var b=document.getElementById(d).getElementsByTagName("img"),a=0;a<b.length;a++)b[a].src=b[a].src.replace(/\/s72\-c/,"/s"+c+""),b[a].width=c}resizeThumb("main",250);
+//]]>
