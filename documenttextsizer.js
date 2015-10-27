24bb93392376cfca6605426b0cb8f0dc2ceffee9
doktertech <doktertech@gmail.com>
2015-10-27 23:21:54 +0700

    Update documenttextsizer.js

--- documenttextsizer.js
+++ documenttextsizer.js
@@ -1,4 +1,3 @@
-//<![CDATA[
 /*------------------------------------------------------------
 	Document Text Sizer- Copyright 2003 - Taewook Kang.  All rights reserved.
 	Coded by: Taewook Kang (txkang.REMOVETHIS@hotmail.com)
@@ -11,4 +10,4 @@
 	this entire copyright notice is included.
 --------------------------------------------------------------*/
 function ts(f,g){if(document.getElementById){var b=document,c=null,a=startSz,d,e,a=a+g;0>a&&(a=0);6<a&&(a=6);startSz=a;(c=b.getElementById(f))||(c=b.getElementsByTagName(f)[0]);c.style.fontSize=szs[a];for(b=0;b<tgs.length;b++)for(e=c.getElementsByTagName(tgs[b]),d=0;d<e.length;d++)e[d].style.fontSize=szs[a]}}var tgs=["div","td","tr"],szs="xx-small x-small small medium large x-large".split(" "),startSz=2;
-//]]>
+
